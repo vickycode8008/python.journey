09ef8321de9164d62a753198062781a3b9497bc5
vickycode8008 <vickyviki339@gmail.com>
2026-08-22 14:38:24 +0530

    id checker

--- week1 code.py/variable.py
+++ week1 code.py/variable.py
@@ -2,4 +2,18 @@ length = 15
 breadth = 20
 area = length * breadth
 print(area)
-#hi
+
+#lets store all data type
+name = "Soap"
+prize = 20
+qty = 10
+print(name, prize, qty)
+
+#id checker
+a = b = c = 100
+print(id(a), id(b), id(c))
+
+
+#id checker
+a = b = c = "Apple"
+print(id(a), id(b), id(c))
