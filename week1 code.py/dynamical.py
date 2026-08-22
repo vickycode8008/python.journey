372e0103069d495136794ceda186d33bc9d16aef
vickycode8008 <vickyviki339@gmail.com>
2026-08-22 15:01:10 +0530

    data type checked

---
+++ week1 code.py/dynamical.py
@@ -0,0 +1,21 @@
+#int 
+a = 15
+print(type(a))
+#float type
+a = 15.80
+print(type(a))
+#string
+a = "Hello World"
+print(type(a))
+#list
+a = [10,20,30]
+print(type(a))
+#tuple
+a = (100,200,300)
+print(type(a))
+#char
+a = "A"
+print(type(a))
+#boolean
+a = True
+print(type(a))
