f6150a4e3884a47314c749af5268610c2df65add
vickycode8008 <vickyviki339@gmail.com>
2026-08-23 22:49:06 +0530

    literals conditions

---
+++ week1 code.py/literals.py
@@ -0,0 +1,7 @@
+#literals should be consider"_" also 
+a = 10
+b = 28_34_56_890
+print(a,b)
+#note: not consider front or end of the digits
+a = 133_
+a = _290
---
+++ week1 code.py/numerical.py
@@ -0,0 +1,11 @@
+#int without giving daya type it consider as what data you give
+a = 10
+b = -20
+print(a*b)
+
+#float 
+a = 20.03
+b = 2.05
+print(a*b)
+
+#bool
