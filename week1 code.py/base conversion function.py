a433f9008a2c2d7c19fedb9f17b78142e9fa3b60
vickycode8008 <vickyviki339@gmail.com>
2026-08-24 17:56:35 +0530

    key point for understanding

--- week1 code.py/base conversion function.py
+++ week1 code.py/base conversion function.py
@@ -1,8 +1,11 @@
+#s1,s2,s3 -- s will be a string
+
 s1 = bin(20)
 s2 = oct(10)
 s3 = hex(10)
 print(s1,s2,s3)
 
+#what if i assign the value in charactor let see
 a = bin(10)
 b = oct(5)
 c = hex(10)
