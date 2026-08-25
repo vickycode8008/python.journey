4bfc29016cf22a2aa430a1fc2f5148d436fe643f
vickycode8008 <vickyviki339@gmail.com>
2026-08-25 18:14:50 +0530

    expression in py

---
+++ python expression.py
@@ -0,0 +1,51 @@
+#area of triangle
+
+length = int(input("Enter length:"))
+breadth = int(input("Enter breadth:"))
+area = length*breadth
+print("Area is : ",area)
+
+
+
+#area of trapisum
+
+side_a = int(input("Enter value of side a = "))
+side_b = int(input("Enter value of side b = "))
+height = int(input("Enter value of height = "))
+area = 1/2*(side_a+side_b)*height
+print("Area is : ",area)
+
+#area of circle
+
+import math
+ 
+r = int(input("Enter radius : "))
+area_of_circle = math.pi *r *r
+print("Area of circle = ",area_of_circle)
+
+
+
+#km to miles
+
+km = int(input("Enter kilo meter: "))
+km_to_mtr = km *0.621371
+print(km_to_mtr)
+
+
+#displacemnt
+
+initial_velocity = int(input("Enter initial velocity:"))
+acceleration = int(input("Enter the accleration:"))
+final_velocity = int(input("Enter final velocity:"))
+
+formula = (v*v - u*u)/(2*a)
+print(formula)
+
+
+#miles to km
+
+mtr = int(input("Enter the meter: "))
+mtr_to_km = mtr *1.60934
+print(mtr_to_km)
+
+#let's see where the journey goo :)
--- week1 code.py/program using expression.py
+++
@@ -1,5 +0,0 @@
-#lets find the area using length and breadth
-length = 15
-breadth = 20
-area = length * breadth
-print("Area is: ",area)
