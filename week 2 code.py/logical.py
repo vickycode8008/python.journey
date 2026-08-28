e63f869a6490fe228008ca2ddd39a879c8eebfe1
VIGNESH <vickyviki339@gmail.com>
2026-08-28 16:15:44 +0530

    and,or logical

--- week 2 code.py/logical.py
+++ week 2 code.py/logical.py
@@ -1,3 +1,18 @@
-a = 4
-b = 10
-print(a+b)
+#using and operator(both are correct - false)
+a = 10
+b = 5
+if a!=10 and b==5:
+     print(True)
+else:
+     print(False)
+
+
+#using or operator(any one should be crt - true)
+
+a = 10
+b = 5
+
+if a!=10 or b==5:
+    print(True)
+else:
+    print(False)
