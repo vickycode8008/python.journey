3c089aa5c554717cd34c9f13e7f26e6c131bab6a
VIGNESH <vickyviki339@gmail.com>
2026-09-16 22:01:29 +0530

    Error changed

--- week 2 code.py/maxium value.py
+++ week 2 code.py/maxium value.py
@@ -7,5 +7,5 @@ while i < n:                                    #check whether the value will be
     x = int(input(""))                           #enter the number
     if x > max :                                   #if condition for check the maxmium value
         max = x                                    # if "x > max" store the value in max
-    else :                                          #else condition
-        print("MAX : ",max)                         #print the value in max 
+print("MAX : ",max)                                       #else condition                    
+                                            #          #print the value in max 
