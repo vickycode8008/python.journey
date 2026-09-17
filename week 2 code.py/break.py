6375d3cf910a5cfe51e218d135bb8cf5ed2c3af1
VIGNESH <vickyviki339@gmail.com>
2026-09-17 18:22:35 +0530

    countinue condition

--- week 2 code.py/break.py
+++ week 2 code.py/break.py
@@ -1,5 +1,5 @@
 i = 0
-while 1>0:
+while i>=0:
     i += 1 
     if i >10:    #if i >10 they stop excuting
         break    #using breaak for to stop the program
---
+++ week 2 code.py/infinite loop.py
@@ -0,0 +1,4 @@
+i = 0
+while i>= 0:
+    i = i + 1 # here it will print infinite number 
+    print(i)
