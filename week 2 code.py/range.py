b5bed7458e632506284f26c38146cde7bcb5f2af
VIGNESH <vickyviki339@gmail.com>
2026-09-22 18:59:04 +0530

    for loop in range

---
+++ week 2 code.py/range.py
@@ -0,0 +1,7 @@
+#basic for loop in range
+for i in range (1,5): #it will print before 5
+    print(i)
+for i in range (1,5,2): #it will print before 5 and it will print first print secound skip 
+    print(i)
+for i in range (10,5,-1): #it will print like reduce 10 to 5 and print -1
+    print(i)
