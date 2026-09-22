cc8a9d07f657d79278c683cb22c99b881b245412
VIGNESH <vickyviki339@gmail.com>
2026-09-22 19:13:31 +0530

    update from sum of range

--- week 2 code.py/using for loop,sum of natural number.py
+++ week 2 code.py/using for loop,sum of natural number.py
@@ -1,4 +1,4 @@
-n = 25      #assoign the number until you want to print
+n = int(input("Enter a number:"))      #assoign the number until you want to print
 sum = 0     #in stating the sum will be must 0
 for i in range(1,n+1): #using range we will assign the formula for adding the natural number
     sum = sum + i    #using sum = sum + i it will increase one b=for every excution
