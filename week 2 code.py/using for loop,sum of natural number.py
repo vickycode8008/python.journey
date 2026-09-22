57230b1cf9b482c7e468e5f17836baa2a8f2353a
VIGNESH <vickyviki339@gmail.com>
2026-09-22 19:09:15 +0530

    sum of natural  numbaer

---
+++ week 2 code.py/using for loop,sum of natural number.py
@@ -0,0 +1,5 @@
+n = 25      #assoign the number until you want to print
+sum = 0     #in stating the sum will be must 0
+for i in range(1,n+1): #using range we will assign the formula for adding the natural number
+    sum = sum + i    #using sum = sum + i it will increase one b=for every excution
+print("The sum of ",n,sum )  #print the total value of natural number
