5588decb58fe6fa3cd8082a9e7f57bff18da577d
VIGNESH <vickyviki339@gmail.com>
2026-09-25 15:26:28 +0530

    using nested loop

--- week 2 code.py/exercise.py
+++ week 2 code.py/exercise.py
@@ -1,3 +1,9 @@
-for i in range(1,4):
- for j in range(1,4):
-  print(i,"-",j) 
+for i in range(1,6):   #using for loop we assign the range value
+    print('*'* i)      #Using the print statement we print the start 
+
+# anthor code 
+for i in range(1,6):
+ for j in range(1,6):
+   if i==j or i>=j:
+    print('*',end='')
+ print('') 
