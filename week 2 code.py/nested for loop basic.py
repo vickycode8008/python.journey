40d0f46a880e43e62fdfafe11ca1f3dea3a661cb
VIGNESH <vickyviki339@gmail.com>
2026-09-25 14:29:05 +0530

    nested loop

---
+++ week 2 code.py/nested for loop basic.py
@@ -0,0 +1,5 @@
+#here using two for loop condition that are nested loop,
+# --it will print the number in loop at second line until the second line completed the loop then goes to first lines
+for i in range(1,4): 
+ for j in range(1,4):
+  print(i,"-",j) 
